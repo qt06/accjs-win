6d68cc40b1a111b4b97ca6e5af7f11d6ec9a3c68
qt06 <qt06.com@139.com>
2020-12-12 21:50:08 +0800

    thhe accjs-win has created

---
+++ MainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace accjs_win
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView2Control = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.SuspendLayout();
+            // 
+            // webView2Control
+            // 
+            this.webView2Control.CreationProperties = null;
+            this.webView2Control.Location = new System.Drawing.Point(10, 10);
+            this.webView2Control.Margin = new System.Windows.Forms.Padding(0);
+            this.webView2Control.Name = "webView2Control";
+            this.webView2Control.Size = new System.Drawing.Size(1900, 1060);
+            this.webView2Control.Source = new System.Uri("http://www.zd.hk/", System.UriKind.Absolute);
+            this.webView2Control.TabIndex = 0;
+            this.webView2Control.Text = "webview";
+            this.webView2Control.ZoomFactor = 1D;
+            this.webView2Control.CoreWebView2Ready += new System.EventHandler<System.EventArgs>(this.webView2Control_CoreWebView2Ready);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1894, 1009);
+            this.Controls.Add(this.webView2Control);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "accjs-win";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2Control;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+namespace accjs_win
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            Environment.SetEnvironmentVariable("WEBVIEW2_USER_DATA_FOLDER", System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Application.ProductName), EnvironmentVariableTarget.Process);
+            InitializeComponent();
+            HandleResize();
+        }
+        private void UpdateTitleWithEvent(string message)
+        {
+            string currentDocumentTitle = this.webView2Control?.CoreWebView2?.DocumentTitle ?? "Uninitialized";
+            this.Text = currentDocumentTitle + " (" + message + ")";
+        }
+
+        #region Event Handlers
+        private void WebView2Control_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            UpdateTitleWithEvent("NavigationStarting");
+        }
+
+        private void WebView2Control_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            UpdateTitleWithEvent("NavigationCompleted");
+        }
+
+        private void WebView2Control_SourceChanged(object sender, CoreWebView2SourceChangedEventArgs e)
+        {
+        }
+
+        private void webView2Control_CoreWebView2Ready(object sender, EventArgs e1)
+        {
+
+            this.webView2Control.CoreWebView2.SourceChanged += CoreWebView2_SourceChanged;
+            this.webView2Control.CoreWebView2.HistoryChanged += CoreWebView2_HistoryChanged;
+            this.webView2Control.CoreWebView2.DocumentTitleChanged += CoreWebView2_DocumentTitleChanged;
+            this.webView2Control.CoreWebView2.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.Image);
+            UpdateTitleWithEvent("CoreWebView2InitializationCompleted succeeded");
+        }
+
+        private void WebView2Control_KeyUp(object sender, KeyEventArgs e)
+        {
+            UpdateTitleWithEvent($"AcceleratorKeyUp key={e.KeyCode}");
+        }
+
+        private void WebView2Control_KeyDown(object sender, KeyEventArgs e)
+        {
+            UpdateTitleWithEvent($"AcceleratorKeyDown key={e.KeyCode}");
+            if (e.KeyCode == Keys.Back)
+            {
+                webView2Control.CoreWebView2.GoBack();
+            }
+        }
+
+        private void WebView2Control_AcceleratorKeyPressed(object sender, Microsoft.Web.WebView2.Core.CoreWebView2AcceleratorKeyPressedEventArgs e)
+        {
+
+        }
+
+        private void CoreWebView2_HistoryChanged(object sender, object e)
+        {
+            // No explicit check for webView2Control initialization because the events can only start
+            // firing after the CoreWebView2 and its events exist for us to subscribe.
+
+            UpdateTitleWithEvent("HistoryChanged");
+        }
+
+        private void CoreWebView2_SourceChanged(object sender, CoreWebView2SourceChangedEventArgs e)
+        {
+            UpdateTitleWithEvent("SourceChanged");
+        }
+
+        private void CoreWebView2_DocumentTitleChanged(object sender, object e)
+        {
+            this.Text = this.webView2Control.CoreWebView2.DocumentTitle;
+            UpdateTitleWithEvent("DocumentTitleChanged");
+        }
+        #endregion
+
+        #region UI event handlers
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            HandleResize();
+        }
+
+        #endregion
+
+        private void HandleResize()
+        {
+            // Resize the webview
+            webView2Control.Size = this.ClientSize - new System.Drawing.Size(webView2Control.Location);
+        }
+
+        
+
+        
+    }
+}
